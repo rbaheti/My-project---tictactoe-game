76097c63463341e35967c888ad0a2e68e422895c
Rashmi Baheti <baheti.rash123@gmail.com>
2018-02-25 20:34:25 -0800

    Basic Game working! Need to implement many things!

--- tictactoe/src/Game.js
+++ tictactoe/src/Game.js
@@ -5,42 +5,135 @@ class Game extends Component {
   constructor() {
     super();
     this.state = {
-      gridNumList: [],
-      gridValue: '_',
-      gridRowIndex: -1,
-      gridColIndex: -1
+      gridValues: [['_', '_', '_'], 
+                   ['_', '_', '_'],   
+                   ['_', '_', '_']],
     };
   }
 
-  handleAddGridValue = () => {
-    // Create an object with 2 properties id and title.
-    // id assigns a unique id to a newTodo.
-    // title holds the text of the newTodo.
-    const newGrid = {};
-    newGrid['id'] = this.state.gridNumList.length;
-    newGrid['value'] = this.state.gridValue;
+  checkIfWon = (value, gridValues) => {
+    // check rows
+    let didWin = 0;
+    for(let row = 0; row < 3; ++row) {
+      for(let col = 0; col < 3; ++col) {
+        if(gridValues[row][col] === value) {
+          didWin++;
+          if(didWin === 3) 
+            return true;
+          continue;
+        } else {
+          didWin = 0;
+          break;
+        }
+      }
+    }
+    // check columns
+    for(let col = 0; col < 3; ++col) {
+      for(let row = 0; row < 3; ++row) {
+        if(gridValues[row][col] === value) {
+          didWin++;
+          if(didWin === 3) 
+            return true;
+          continue;
+        } else {
+          didWin = 0;
+          break;
+        }
+      }
+    }
+    // check diagonally
+    if(gridValues[0][0] === value && gridValues[1][1] === value && gridValues[2][2] === value)
+      return true;
+    if(gridValues[0][2] === value && gridValues[1][1] === value && gridValues[2][0] === value)
+      return true;
+    // return false if 'value' didn't win
+    return false;
+  }
 
-    // Create an array newTodoList.
-    // push the value of new todo item to this array
-    const newGridNumList = this.state.gridNumList;
-    newGridNumList.push(newGrid);
-    this.setState({ gridNumList:newGridNumList, gridValue: '_' });  
-  };
+  computersNextMove = (gridValues) => {
+    // Find a cell where 'X' can win.
+    for(let row = 0; row < 3; ++row) {
+      for(let col = 0; col < 3; ++col) {
+        if(gridValues[row][col] === '_') {
+          gridValues[row][col] = 'X';
+          if(this.checkIfWon('X', gridValues)) {
+            gridValues[row][col] = 'O';
+            return gridValues;
+          }
+          gridValues[row][col] = '_';
+        }
+      }
+    }
+
+    // Find a cell where 'O' can win.
+    for(let row = 0; row < 3; ++row) {
+      for(let col = 0; col < 3; ++col) {
+        if(gridValues[row][col] === '_') {
+          gridValues[row][col] = 'O';
+          if(this.checkIfWon('O', gridValues)) {
+            gridValues[row][col] = 'O';
+            return gridValues;
+          }
+          gridValues[row][col] = '_';
+        }
+      }
+    }
+
+    // Place 'O' anywhere.
+    for(let row = 0; row < 3; ++row) {
+      for(let col = 0; col < 3; ++col) {
+        if(gridValues[row][col] === '_') {
+          gridValues[row][col] = 'O';
+          return gridValues;
+        }
+      }
+    }
+  }
+
+  handleOnClickGrid = (row, col) => {
+    return () => { // anonymous arrow function
+      const tempGridValues = this.state.gridValues.slice();
+      console.log("tempGridValues: ", tempGridValues);
+
+      if(tempGridValues[row][col] === '_') {
+        tempGridValues[row][col] = 'X';
+        this.setState({gridValue: tempGridValues});
+      } else {
+        return;
+      }
+
+      // check if X won
+      if(this.checkIfWon('X', tempGridValues)) {
+        console.log("You won!!!!!");
+        return;
+      }
+
+      const updatedGridValues = this.computersNextMove(tempGridValues);
+      this.setState({gridValue: updatedGridValues});
+        
+      // check if 'O' won
+      if(this.checkIfWon('O', tempGridValues)) {
+        console.log("Computer won!");
+        return;
+      }
+    }
+  }
 
   render() {
     const grid_rows = [];
     let value = 1;
     for(let row = 0; row < 3; ++row) {
       for(let col = 0; col < 3; ++col) {
-        grid_rows.push(<div className="grid-item">{'_'}</div>);
+        grid_rows.push(<div key={value} onClick={this.handleOnClickGrid(row, col)} 
+          className="grid-item" >{this.state.gridValues[row][col]}</div>);
         ++value;
       }
     }
+
     return (
       <div className="grid-container">
         <div className="grid">
           {grid_rows}
-          <input onClick={this.createUser} />
         </div>
       </div>
     );
